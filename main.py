5fb883903a56c3d5a755eca86bfce055927ed289
dionisio202 <solisedison@outlook.com>
2025-05-10 23:43:14 -0500

    added corse

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask
 from app.auth.routes import auth_bp,register_bp
 from app.api.routes.files import files_bp
 from app.api.routes.users import users_bp 
+from flask_cors import CORS
+
 # Importa otros blueprints (por ejemplo, de users, files, etc.) según tu estructura
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 
 # Registrar el blueprint de autenticación con el prefijo '/auth'
 app.register_blueprint(auth_bp)
